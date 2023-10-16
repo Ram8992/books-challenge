6901453c37d9d08ac6d50d822abd04ed53a7c67b
unknown <catrielramiro@gmail.com>
2023-10-15 21:56:45 -0300

    UserLoggedMiddleware

---
+++ middlewares/userLoggedMiddleware.js
@@ -0,0 +1,11 @@
+function userLoggedMiddleware (req, res, next) {
+    res.locals.isLoged = false;
+  
+    if( req.session.userLog) {
+      res.locals.isLoged = true;
+      res.locals.userLog = req.session.userLog;
+    }
+    next();
+  }
+  
+  module.exports = userLoggedMiddleware
